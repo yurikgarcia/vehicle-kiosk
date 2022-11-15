af9634ad075b25ac854185b15147d31047eebc5c
adpears94 <pears.avery@gmail.com>
2022-11-15 10:59:52 -0500

    set up

---
+++ api/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 8080
+
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello World!');
+})
+
+
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+})
---
+++ ui/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <div className="App">
+      <h1> Search Gate Tracking Mother Fucker </h1>
+    </div>
+  );
+};
